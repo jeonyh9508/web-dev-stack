b871ccff16e401e7dd14e8a61ce895a500a2ee11
jeonyh9508 <jeonyh95@naver.com>
2025-05-28 17:52:39 +0900

    5/28

--- 03_JavaScript/09_이벤트.js
+++ 03_JavaScript/09_이벤트.js
@@ -41,10 +41,117 @@ window.addEventListener("DOMContentLoaded", () => {
   });
 
   // 폼 이벤트
+  // input 실시간 반영
   const input = document.querySelector("#input");
+  const inputResult = document.querySelector("#inputResult");
   input.addEventListener("input", (e) => {
     console.log(e.target.value);
-    const p = document.querySelector("#inputResult");
-    p.innerHTML = e.target.value;
+
+    inputResult.innerHTML = e.target.value;
   });
+  // select 선택값 출력
+  const select = document.querySelector("#select");
+  const selectResult = document.querySelector("#selectResult");
+  select.addEventListener("change", (e) => {
+    console.log(e.target.value);
+    selectResult.textContent = e.target.value;
+  });
+
+  const form = document.querySelector("#form");
+  const inputText = document.querySelector("#form input");
+  form.addEventListener("submit", (e) => {
+    // 값이 없는 경우만 전송 불가, 값이 있으면 전송
+    // console.log(inputText.value);
+    if (inputText.value === "") {
+      e.preventDefault();
+    }
+  });
+
+  // 키보드 이벤트 : keydown, keypress, keyup
+
+  const keyboard = document.querySelector("#keyboard");
+  const keyResult = document.querySelector("#keyResult");
+  keyboard.addEventListener("keydown", (e) => {
+    console.log(e.key);
+    keyResult.textContent = e.key;
+    if (e.key == "Enter") {
+      alert("엔터");
+    }
+  });
+
+  // 박스 움직이기
+  const move = document.querySelector(".moveBox");
+  let x = 0,
+    y = 0;
+  document.addEventListener("keydown", (e) => {
+    // console.log(e.key);
+    if (e.key === "ArrowUp") {
+      // top이 - 인 경우
+      y -= 10;
+    } else if (e.key === "ArrowDown") {
+      y += 10;
+    } else if (e.key === "ArrowLeft") {
+      x -= 10;
+    } else if (e.key === "ArrowRight") {
+      x += 10;
+    }
+    move.style.top = `${y}px`;
+    move.style.left = `${x}px`;
+  });
+
+  // 스크롤 이벤트
+  const wheel = document.querySelector("#wheel");
+  const scroll = document.querySelector("#scroll");
+
+  window.addEventListener("wheel", (e) => {
+    // console.log(e.deltaY);
+    // 휠을 올리면 - , 휠을 내리면 +
+    if (e.deltaY < 0) {
+      wheel.textContent = "휠 위로 스크롤 중";
+    } else if (e.deltaY > 0) {
+      wheel.textContent = "휠 아래로 스크롤 중";
+    }
+  });
+  window.addEventListener("scroll", () => {
+    // 전체 높이
+    // console.log("scrollHeight : " + document.documentElement.scrollHeight);
+    // 보이는 화면의 높이
+    // console.log("innerHeight : " + window.innerHeight);
+    // console.log("scrollY : " + window.scrollY);
+    // scrollHeight === innerHeight + scrollY
+    // scrollHeight - innerHeight : 스크롤을 내리는 총 높이
+    const totalHeight =
+      document.documentElement.scrollHeight - window.innerHeight;
+    const percent = (window.scrollY / totalHeight) * 100;
+    // console.log(percent);
+    scroll.style.width = `${percent}%`;
+  });
+
+  // 이벤트 위임
+  // 이미지가 클릭될 때마다 해당 이미지 지우기
+  // 1. d이미지마다 이벤트 걸려면 반복문
+  const img = document.querySelectorAll(".container img");
+  console.log(img);
+  /*
+  for (let i = 0; i < img.length; i++) {
+    // console.log(img[i]);
+    img[i].addEventListener("click", (e) => {
+      // img[i].style.display = "none";
+
+      console.log(e.currentTarget);
+      e.currentTarget.style.display = "none";
+    });
+  }
+  */
+
+  // 2.부모인 컨테이너에 이벤트 걸어서
+  const container = document.querySelector(".container");
+  const removeHandler = (e) => {
+    console.log("target : ", e.target); // 내가 클릭한 이미지
+    console.log("current : ", e.currentTarget); // 이벤트 걸린 본인
+    if (e.target !== e.currentTarget) {
+      e.target.style.display = "none";
+    }
+  };
+  container.addEventListener("click", removeHandler);
 });
